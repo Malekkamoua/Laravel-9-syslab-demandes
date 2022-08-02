1990bd8632fa8a789918c16a8e07025aaef0ca49
Malek Kamoua <36742939+Malekkamoua@users.noreply.github.com>
2022-08-02 18:19:56 +0100

    updates some views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
 
 <div class="container mt--8 pb-5">
     <div class="row justify-content-center">
-        <div class="col-lg-5 col-md-7">
+        <div class="col-lg-5 col-md-7" style="position: relative;top: -140px;">
             <div class="card bg-secondary shadow border-0">
                 <div class="card-header bg-transparent pb-5">
                     <div class="text-muted text-center mt-2 mb-3"><small>{{ __('Sign in with') }}</small></div>
